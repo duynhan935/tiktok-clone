48f5769ce4949bfb6675d445e837269a334e109a
duynhan935 <tranduynhan2005ct@gmail.com>
2025-03-02 20:58:49 +0700

    Dựng khung Layout cho các trang mặc định, profile, following, upload

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+// Layouts
+import { HeaderOnly } from "~/components/Layout";
+
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Upload from "~/pages/Upload";
+
+const publicRoutes = [
+    {
+        path: "/",
+        component: Home,
+    },
+    {
+        path: "/following",
+        component: Following,
+    },
+    {
+        path: "/profile",
+        component: Profile,
+    },
+    {
+        path: "/upload",
+        component: Upload,
+        layout: HeaderOnly,
+    },
+];
+// Những router dành cho những trang không cần đăng nhập
+
+const privateRoutes = {};
+// Những router dành cho những trang cần đăng nhập
+
+export { publicRoutes, privateRoutes };
